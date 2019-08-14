a987149d0011a9c38f78e19a36a9fe79f9787e12
rraabe <49948103+rraabe@users.noreply.github.com>
2019-08-14 15:51:08 -0400

    Changed the dialog to flow better. Tested with follow-up intents.

--- functions/index.js
+++ functions/index.js
@@ -3,15 +3,12 @@ const functions = require("firebase-functions");
 
 process.env.DEBUG = "dialogflow:debug"; // enables lib debugging statements
 
-//YNAB - Change to use OAuth
 const ynab = require("ynab");
 
-const app = dialogflow({ debug: true }); //is this redundant with line 4
+const app = dialogflow({ debug: true }); //is this redundant with line 4?
 
 app.intent("Default Welcome Intent", conv => {
   conv.ask(`What information can I get for you today?`);
-  // Complete your fulfillment logic and
-  // send a response when the function is done executing
 });
 
 //Given a nameKey (category), search an array of objects for an object with the name of nameKey and return that object.
@@ -25,18 +22,14 @@ function search(nameKey, myArray) {
 }
 //This intent is triggered by giving a category. It fetches all the budget categories from the default budget for the current month.
 //The returned categories are parsed down to the name, balance, and budgeted amount (we may not need this). We then use a search function
-//to search for the category name passed in and return an object (or objects) that have the same name. We then speak the category name and balance.
+//to search for the category name passed in and return an object that has the same name. We then speak the category name and balance.
 app.intent("get balance", (conv, { categories }) => {
   //We have to instantiate the new YNAB API after the OAuth token has been stored in conv.user.raw.accessToken.
   const ynabAPI = new ynab.API(conv.user.raw.accessToken);
 
-  //Leave the if/else commented out for now. This way we force a fetch each time. This let's us update our information between requests. Eventually
-  //this should be a lamda request.
-
-  // if (!conv.data.balances) {
   return ynabAPI.months.getBudgetMonth("default", "current").then(r => {
     const ynabCategories = r.data.month.categories;
-    //conv.data.balances will store all the budget information will in the conversation. Use this to check if the data has already been fetched.
+    //conv.data.balances will store all the budget information will in the conversation.
     conv.data.balances = ynabCategories.map(c => {
       return {
         name: c.name.toLowerCase(),
@@ -57,45 +50,31 @@ app.intent("get balance", (conv, { categories }) => {
       conv.ask(
         `You have all $${categoryObj.budgeted} of your ${
           categoryObj.name
-        } budget remaining.`
+        } budget remaining. Do you want to check another balance?`
       );
       // If a user has spent some, but is still in the green.
     } else if (categoryObj.balance >= 0) {
       conv.ask(
         `You have $${categoryObj.balance} remaining of $${
           categoryObj.budgeted
-        } budgeted for ${categoryObj.name}.`
+        } for ${
+          categoryObj.name
+        }. Do you want to check another balance?`
       );
       // If a user has gone over their budget.
     } else if (categoryObj.balance < 0) {
       conv.ask(
         `You are over your ${
           categoryObj.name
-        } budget by $${-categoryObj.balance}`
+        } budget by $${-categoryObj.balance}. Do you want to check another balance?`
       );
       // In the case that no category is found, this will ask the user to try again.
     } else {
       conv.ask(
-        `I'm sorry, I couldn't find a matching budget. Please try again.`
+        `I'm sorry, I couldn't find a category called ${categories}. Do you want to check another balance?`
       );
     }
   });
-  // } else {
-  //   //Find in the logs if a second request uses the stored budget or fetches new data.
-  //   //If we're adding new transactions this will have to be removed or we'll need to use delta requests.
-  //   const categoryObj = search(categories, conv.data.balances);
-  //   conv.ask(
-  //     `You have $${categoryObj.balance} remaining in your ${
-  //       categoryObj.name
-  //     } budget`
-  //   );
-  // }
 });
-
-// if we want to move sign in from the start of the Action we will have to use somthing like this.
-// Intent that starts the account linking flow.
-// app.intent('Start Signin', (conv) => {
-//     conv.ask(new SignIn('To get your account details'));
-//   });
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
