1bf72f8b77a4766cfb988d6dbc8f9621fb26654b
DoctorGrim <estlund.marshall@gmail.com>
2019-08-15 13:25:25 -0400

    minor dlg fix

--- functions/index.js
+++ functions/index.js
@@ -7,21 +7,17 @@ const ynab = require("ynab");
 
 const app = dialogflow({ debug: true }); //is this redundant with line 4?
 
-<<<<<<< HEAD
-app.intent('Default Welcome Intent', (conv) => {
-  conv.close(`${conv.user.raw.accessToken}hello world`);
-  // Complete your fulfillment logic and
-  // send a response when the function is done executing
-=======
 app.intent("Default Welcome Intent", conv => {
-  conv.ask(`What information can I get for you today?`);
->>>>>>> 9d3b306354c290092a949e80c483d8e2275fc095
+  conv.ask(` What information can I get for you today?`);
 });
 
 //Given a nameKey (category), search an array of objects for an object with the name of nameKey and return that object.
 function search(nameKey, myArray) {
   for (var i = 0; i < myArray.length; i++) {
-    if (myArray[i].name.replace(/[^a-zA-Z0-9 ]/g,'') == nameKey.replace(/[^a-zA-Z0-9 ]/g,'')) {
+    if (
+      myArray[i].name.replace(/[^a-zA-Z0-9 ]/g, "") ==
+      nameKey.replace(/[^a-zA-Z0-9 ]/g, "")
+    ) {
       return myArray[i];
     }
   }
@@ -34,56 +30,58 @@ app.intent("get balance", (conv, { categories }) => {
   //We have to instantiate the new YNAB API after the OAuth token has been stored in conv.user.raw.accessToken.
   const ynabAPI = new ynab.API(conv.user.raw.accessToken);
 
-  return ynabAPI.months.getBudgetMonth("default", "current").then(r => {
-    const ynabCategories = r.data.month.categories;
-    //conv.data.balances will store all the budget information will in the conversation.
-    conv.data.balances = ynabCategories.map(c => {
-      return {
-        name: c.name.toLowerCase(),
-        balance: ynab.utils
-          .convertMilliUnitsToCurrencyAmount(c.balance, 2)
-          .toFixed(2),
-        budgeted: ynab.utils
-          .convertMilliUnitsToCurrencyAmount(c.budgeted, 2)
-          .toFixed(2)
-      };
-    });
-    const categoryObj = search(categories, conv.data.balances);
-    if (
-      categoryObj.balance === categoryObj.budgeted &&
-      categoryObj.balance != undefined
-    ) {
-      // If the user has all of their budgeted available.
+  return ynabAPI.months
+    .getBudgetMonth("default", "current")
+    .then(r => {
+      const ynabCategories = r.data.month.categories;
+      //conv.data.balances will store all the budget information will in the conversation.
+      conv.data.balances = ynabCategories.map(c => {
+        return {
+          name: c.name.toLowerCase(),
+          balance: ynab.utils
+            .convertMilliUnitsToCurrencyAmount(c.balance, 2)
+            .toFixed(2),
+          budgeted: ynab.utils
+            .convertMilliUnitsToCurrencyAmount(c.budgeted, 2)
+            .toFixed(2)
+        };
+      });
+      const categoryObj = search(categories, conv.data.balances);
+      if (
+        categoryObj.balance === categoryObj.budgeted &&
+        categoryObj.balance != undefined
+      ) {
+        // If the user has all of their budgeted available.
+        conv.ask(
+          `You have $${categoryObj.budgeted} of your ${
+            categoryObj.name
+          } budget remaining. Do you want to check another balance?`
+        );
+        // If a user has spent some, but is still in the green.
+      } else if (categoryObj.balance >= 0) {
+        conv.ask(
+          `You have $${categoryObj.balance} avalable in your ${
+            categoryObj.name
+          } budget. Do you want to check another balance?`
+        );
+        // If a user has gone over their budget.
+      } else if (categoryObj.balance < 0) {
+        conv.ask(
+          `You are over your ${
+            categoryObj.name
+          } budget by $${-categoryObj.balance}. Do you want to check another balance?`
+        );
+        // In the case that no category is found, this will ask the user to try again.
+      } else {
+        conv.ask(
+          `I'm sorry, I couldn't find a category called ${categories}. Do you want to check another balance?`
+        );
+      }
+    })
+    .catch(e => {
       conv.ask(
-        `You have all $${categoryObj.budgeted} of your ${
-          categoryObj.name
-        } budget remaining. Do you want to check another balance?`
+        `There was an error fetching your information. Please try again later. Do you want to check another balance?`
       );
-      // If a user has spent some, but is still in the green.
-    } else if (categoryObj.balance >= 0) {
-      conv.ask(
-        `You have $${categoryObj.balance} remaining of $${
-          categoryObj.budgeted
-        } for ${
-          categoryObj.name
-        }. Do you want to check another balance?`
-      );
-      // If a user has gone over their budget.
-    } else if (categoryObj.balance < 0) {
-      conv.ask(
-        `You are over your ${
-          categoryObj.name
-        } budget by $${-categoryObj.balance}. Do you want to check another balance?`
-      );
-      // In the case that no category is found, this will ask the user to try again.
-    } else {
-      conv.ask(
-        `I'm sorry, I couldn't find a category called ${categories}. Do you want to check another balance?`
-      );
-    }
-  })
-  .catch(e => {
-      conv.ask(`There was an error fetching your information. Please try again later. Do you want to check another balance?`);
     });
 });
 
