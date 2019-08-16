27e85cd0a8329aa5b59598a1824b8faf2749f6c2
Jake Barrington <mjakebarrington@gmail.com>
2019-08-16 13:29:16 -0400

    fixed issues related to emojis/whitespace

--- functions/index.js
+++ functions/index.js
@@ -15,8 +15,8 @@ app.intent("Default Welcome Intent", conv => {
 function search(nameKey, myArray) {
   for (var i = 0; i < myArray.length; i++) {
     if (
-      myArray[i].name.replace(/[^a-zA-Z0-9 ]/g, "") ==
-      nameKey.replace(/[^a-zA-Z0-9 ]/g, "")
+      myArray[i].name.replace(/[^a-zA-Z0-9 ]/g, "").replace(/([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '').replace(' ', '') ==
+      nameKey.replace(/[^a-zA-Z0-9 ]/g, "").replace(/([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '').replace(' ', '')
     ) {
       return myArray[i];
     }
